e43743699f2070adaf9ae04b675461c94a12431a
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-09-18 11:48:07 +0700

    aman

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,10 @@ const LandingPage = () => {
     <div className='container mx-auto ' >
       <div className="flex flex-col items-center justify-center min-h-[100vh] gap-3">
         <img className='w-32 border-none' src="https://maukuliah.ap-south-1.linodeobjects.com/logo/1701313060-enuc1snQTO.jpg" alt="" />
+
         <h1>Masuk ke pendataan mahasiswa</h1>
-        <Link href='/home' className='bg-blue-500 text-white px-3 py-2 rounded-md' >Masuk</Link>
+        <Link href='/home' className='bg-blue-500 text-white px-3 py-2 rounded-md' >Masuk halaman mahasiswa</Link>
+        <Link href='/create_articles' className='bg-blue-500 text-white px-3 py-2 rounded-md' >Masuk halaman artikel</Link>
       </div>
     </div>
 
